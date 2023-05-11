51c3c2cf8497af3e0739a52e06a81a8c83c4d0dc
Imran Nayer <imrannayer@google.com>
2023-05-11 10:14:21 -0500

    updated

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 
 resource "google_storage_bucket" "my_bucket" {
   project                     = "mysintest"
-  name                        = "my-tf122226565test
+  name                        = "my-tf122226565test"
   location                    = "US"
   force_destroy               = true
   uniform_bucket_level_access = true
