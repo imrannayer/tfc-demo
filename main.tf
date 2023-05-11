0f931e46b8292813a52aaa6b210fbaa886c4ce30
Imran Nayer <imrannayer@google.com>
2023-05-10 22:33:45 -0500

    updated

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  project                     = "mysintest"
+  name                        = "my-tf122226565"
+  location                    = "US"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+
+  # public_access_prevention = "enforced"
+}
+
