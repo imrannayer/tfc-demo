b9d647e7b6f061a717e398cbb058e3cbbaca3dc3
Imran Nayer <imrannayer@google.com>
2023-05-10 23:05:52 -0500

    updated

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "google_storage_bucket" "my_bucket" {
   project                     = "mysintest"
   name                        = "my-tf122226565"
   location                    = "US"
-  force_destroy               = true
+  force_destroy               = false
   uniform_bucket_level_access = true
 
   # public_access_prevention = "enforced"
